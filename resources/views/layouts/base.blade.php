02f01ba95e5a69e8e44b92be25267a75d3571309
Vlad Ageysha <ander123fakk@gmail.com>
2022-10-06 13:12:41 +0300

    Add exit btn

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -13,6 +13,10 @@
             <a class="navbar__link" href="{{ route('index') }}">Главная</a>
             <a class="navbar__link" href="{{ route('login') }}">Вход</a>
             <a class="navbar__link" href="{{ route('register') }}">Регистрация</a>
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <input type="submit" class="navbar__link" value="Выход">
+            </form>
         </div>
     </nav>
     @yield('main')
