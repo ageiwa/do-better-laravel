ffcdbedbb87f0219346e4033d44db548363e5758
Vlad Ageysha <ander123fakk@gmail.com>
2022-10-31 14:07:05 +0300

    Auth is done

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use App\Http\Controllers\PostController;
 */
 
 Route::get('/', [PostController::class, 'index'])->name('index');
-Route::get('/{post}', [PostController::class, 'detail'])->name('detail');
 
 Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/{post}', [PostController::class, 'detail'])->name('detail');
